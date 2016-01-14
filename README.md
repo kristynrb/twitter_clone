d3c447061ac09e9fe3e5fdcb0ad4501361ce2294
Kristyn Bryan <kristynrb@gmail.com>
2016-01-14 08:43:26 -0500

    update to Readme doc. Initial installation of mongoose

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Overall environment setup includes installing packages for:
 - method-override
 - ejs
 - express-ejs-layouts
+
+## Phase 2
+
+Database setup:
+- Add in mongoose
+- Create schema for Tweets
+
+Setup routes and pages for Create & Read All:
+- GET 'tweets/new' should be a form for tweets
+- POST 'tweets' should create a new tweet
+- GET 'tweets' should show every tweet 
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-var express = require('express'),
-    server = express(),
-    ejs = require('ejs'),
+var express           = require('express'),
+    server            = express(),
+    ejs               = require('ejs'),
     expressEjsLayouts = require('express-ejs-layouts'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+    bodyParser        = require('body-parser'),
+    methodOverride    = require('method-override'),
+    mongoose          = require('mongoose');
+
+var Tweet = mongoose.model("tweet", {
+  author: String,
+  content: { type: String, maxlength: 140 }
+});
 
 server.set('view engine', 'ejs');
-server.set('veiws', './views');
+server.set('views', './views');
 
 server.use(express.static('./public'));
 server.use(expressEjsLayouts);
